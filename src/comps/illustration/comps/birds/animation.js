ef6961effee67675b54936cd784ad3dece655ab3
sarovin <sarovin86@gmail.com>
2018-05-24 11:07:50 +0200

    REMOVE birds animation

--- src/comps/illustration/comps/birds/animation.js
+++ src/comps/illustration/comps/birds/animation.js
@@ -81,7 +81,7 @@ const Left = styled.g`
 `;
 
 const Animation = ({ transform, delay, reverse }) => (
-  <Wrapper delay={delay} reverse={reverse}>
+  <g delay={delay} reverse={reverse}>
     <g
       transform={transform}
       id="BJgHwWPhAf_SJCjDDh0M"
@@ -89,25 +89,25 @@ const Animation = ({ transform, delay, reverse }) => (
       data-animator-type="1"
     >
       <g id="BJgHwWPhAf">
-        <Left>
-          <g id="BJ-HwZwhRG_HJnLGDhRz" data-animator-group="true" data-animator-type="1">
+        <g>
+          <g id="BJ-HwZwhRG_HJnLGDhRz">
             <path
               id="BJ-HwZwhRG"
               d="M77.3,47.8C74,45.6,70,44.4,66,44.5c-4-0.1-7.9,1-11.3,3.2c-2.9,2-4.7,4.8-4.7,8"
             />
           </g>
-        </Left>
-        <Right>
-          <g id="B1zHvWPn0f_Bk57zvnAf" data-animator-group="true" data-animator-type="1">
+        </g>
+        <g>
+          <g id="B1zHvWPn0f_Bk57zvnAf">
             <path
               id="B1zHvWPn0f"
               d="M50,55.7c0-3.1-1.8-6-4.6-8c-3.3-2.3-7.3-3.5-11.3-3.4c-4-0.1-8,1-11.3,3.2"
             />
           </g>
-        </Right>
+        </g>
       </g>
     </g>
-  </Wrapper>
+  </g>
 );
 
 Animation.propTypes = propTypes;
